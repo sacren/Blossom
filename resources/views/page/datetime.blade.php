c0fc69df47ad6f45b7990cbc701d8060f6c0e6c5
Jean Sacren <sakiwit@gmail.com>
2024-11-06 17:32:24 -0700

    Add change of time zone
    Use date_default_timezone_set and date_default_timezone_get to change
    and show time zones.

--- resources/views/page/datetime.blade.php
+++ resources/views/page/datetime.blade.php
@@ -10,15 +10,27 @@ Datetime
 
 date_default_timezone_set('America/Denver'); // Set timezone first
 
+echo date_default_timezone_get() . '<br>';
+
 $currentTime = time(); // Unix timestamp
 
 echo $currentTime . '<br>';
 
-echo $currentTime + 5 * 60 * 60 * 24 . '<br>'; // 5 days from now
+$fiveDaysFromNow = time() + 5 * 24 * 60 * 60; // 5 days from now
+
+echo $fiveDaysFromNow . '<br>';
 
 echo $currentTime - 1 * 60 * 60 * 24 . '<br>'; // 1 day ago
 
 echo date('m/d/Y g:ia', $currentTime) . '<br>'; // use date() to format a timestamp
+
+echo date('m/d/Y g:ia', $fiveDaysFromNow) . '<br>';
+
+date_default_timezone_set('UTC'); // set timezone back to UTC
+
+echo date_default_timezone_get() . '<br>';
+
+echo date('m/d/Y g:ia', $currentTime) . '<br>';
 
 @endphp
 
