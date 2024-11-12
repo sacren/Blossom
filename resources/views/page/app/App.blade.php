0ff5072e14d4c82bb9a664043c49e7d348377c40
Jean Sacren <sakiwit@gmail.com>
2024-11-11 23:58:45 -0700

    Add getTransactionFiles function
    The function searches the given path for the list of files.

--- resources/views/page/app/App.blade.php
+++ resources/views/page/app/App.blade.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
+function getTransactionFiles(string $path): array
+{
+    $files = [];
+
+    foreach (scandir($path) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $file;
+    }
+
+    return $files;
+}
+
 @endphp
