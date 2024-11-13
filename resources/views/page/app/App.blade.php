0ccbfb7c85f61749fc6f4e4061302e20c4379641
Jean Sacren <sakiwit@gmail.com>
2024-11-12 20:11:19 -0700

    Add getTransactions function
    Build array out of the given CSV file.  Filter out the entries that are
    not part of the transaction.

--- resources/views/page/app/App.blade.php
+++ resources/views/page/app/App.blade.php
@@ -17,4 +17,34 @@ function getTransactionFiles(string $path): array
     return $files;
 }
 
+/**
+ * Scan the file in csv and save transactions in array
+ *
+ * @param string $fileName
+ *
+ * @return array
+ */
+function getTransactions(string $fileName): array
+{
+    if (!file_exists($fileName)) {
+        trigger_error('File not found: ' . $fileName, E_USER_ERROR);
+    }
+
+    $filepointer = fopen($fileName, 'r');
+
+    $transactions = [];
+
+    while (($transaction = fgetcsv($filepointer)) !== false) {
+        if ( $transaction[0] === 'Date') {
+            continue;
+        }
+
+        $transactions[] = $transaction;
+    }
+
+    fclose($filepointer);
+
+    return $transactions;
+}
+
 @endphp
