0c4d48b9f9d0a73f2b39322390921c71a0b3aa1b
Jean Sacren <sakiwit@gmail.com>
2024-11-16 21:00:12 -0700

    Access values of array and object
    Show the difference of accessing values of array and object.

--- resources/views/page/arrayandobject.blade.php
+++ resources/views/page/arrayandobject.blade.php
@@ -8,12 +8,32 @@ declare(strict_types = 1);
 
 @php
 
-$tmp = new ArrayAndObject();
+$str = new ArrayAndObject();
 
-var_dump($tmp->toArray());
+$arr = $str->toArray();
+var_dump($arr);
 echo '<br>';
 
-var_dump($tmp->toObject());
+var_dump($arr['a']); // int(1)
+echo '<br>';
+
+var_dump($arr['b']); // int(2)
+echo '<br>';
+
+var_dump($arr['c']); // int(3)
+echo '<br>';
+
+$obj = $str->toObject();
+var_dump($obj);
+echo '<br>';
+
+var_dump($obj->a); // int(1)
+echo '<br>';
+
+var_dump($obj->b); // int(2)
+echo '<br>';
+
+var_dump($obj->c); // int(3)
 echo '<br>';
 
 @endphp
