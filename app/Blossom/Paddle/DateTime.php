a3ee9b3b6651143d3f39e21411b17de7441054bd
Jean Sacren <sakiwit@gmail.com>
2024-11-22 16:57:46 -0700

    Add DateTime class in App\Blossom\Paddle namespace
    Build DateTime class in App\Blossom\Paddle namespace.  Transaction of
    Paddle has the same namespace as DateTime.  Use DateTime class within
    Transaction, but global DateTime is used with 'use DateTime;' override.

---
+++ app/Blossom/Paddle/DateTime.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Blossom\Paddle;
+
+class DateTime
+{
+    /**
+     * Create DateTime class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
