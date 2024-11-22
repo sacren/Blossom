b078ae9fecf1e27ebb311c862687793c282d31fd
Jean Sacren <sakiwit@gmail.com>
2024-11-22 16:02:10 -0700

    Include DateTime of global namespace
    Using the keyword 'use' and the class name itself is a good way of
    accessing the class of global namespace.

--- app/Blossom/Paddle/Transaction.php
+++ app/Blossom/Paddle/Transaction.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Blossom\Paddle;
 
+use DateTime;
+
 class Transaction
 {
     /**
@@ -14,7 +16,7 @@ class Transaction
         // object(App\Blossom\Paddle\CustomerProfile)#297 (0) { }
         var_dump(new CustomerProfile());
         echo '<br>';
-        var_dump(new \DateTime());
+        var_dump(new DateTime());
         echo '<br>';
     }
 }
