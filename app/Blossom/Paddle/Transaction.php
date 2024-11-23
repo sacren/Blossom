2d7bac83b01daa2f3577f356d064e9a70614cce6
Jean Sacren <sakiwit@gmail.com>
2024-11-22 21:06:50 -0700

    Use explode in global namespace
    First the function is searched in local namespace.  Not found, it is
    searched in global namespace.

--- app/Blossom/Paddle/Transaction.php
+++ app/Blossom/Paddle/Transaction.php
@@ -18,5 +18,8 @@ class Transaction
         echo '<br>';
         var_dump(new DateTime());
         echo '<br>';
+        // array(2) { [0]=> string(2) "Hi" [1]=> string(8) " Paddle!" }
+        var_dump(explode(',', 'Hi, Paddle!'));
+        echo '<br>';
     }
 }
