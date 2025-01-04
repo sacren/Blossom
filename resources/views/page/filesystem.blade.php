32edd6693d604a7c04096c1c58ec75ae574ff403
Jean Sacren <sakiwit@gmail.com>
2025-01-03 21:12:58 -0700

    Use app-layout for page.filesystem view
    Use app-layout in lieu of custom layout for page.filesystem view.
    
    Replace title slot with header slot and format the whole view.

--- resources/views/page/filesystem.blade.php
+++ resources/views/page/filesystem.blade.php
@@ -1,59 +1,59 @@
 @php
-declare(strict_types=1);
+    declare(strict_types=1);
 @endphp
 
-<x-layout>
+<x-app-layout>
 
-<x-slot:title>
-Filesystem
-</x-slot>
+    <x-slot:header>
+        Filesystem
+    </x-slot:header>
 
-@php
+    @php
 
-/** filesystem */
+        /** filesystem */
 
-$dir = scandir(__DIR__); // array
+        $dir = scandir(__DIR__); // array
 
-var_dump($dir);
-echo '<br>';
+        var_dump($dir);
+        echo '<br>';
 
-var_dump($dir[0]); // current directory
-echo '<br>';
+        var_dump($dir[0]); // current directory
+        echo '<br>';
 
-var_dump($dir[1]); // parent directory
-echo '<br>';
+        var_dump($dir[1]); // parent directory
+        echo '<br>';
 
-var_dump(is_file($dir[0])); // bool false
-echo '<br>';
+        var_dump(is_file($dir[0])); // bool false
+        echo '<br>';
 
-var_dump(is_dir($dir[1])); // bool true
-echo '<br>';
+        var_dump(is_dir($dir[1])); // bool true
+        echo '<br>';
 
-$filename = resource_path('css/app.css');
+        $filename = resource_path('css/app.css');
 
-if (file_exists($filename)) {
-    clearstatcache();
-    echo filesize($filename) . '<br>';
-} else {
-    echo 'File not found';
-    return;
-}
+        if (file_exists($filename)) {
+            clearstatcache();
+            echo filesize($filename) . '<br>';
+        } else {
+            echo 'File not found';
+            return;
+        }
 
-$filepointer = fopen($filename, 'r');
+        $filepointer = fopen($filename, 'r');
 
-while (($line = fgets($filepointer)) !== false) {
-    echo $line . '<br>';
-}
+        while (($line = fgets($filepointer)) !== false) {
+            echo $line . '<br>';
+        }
 
-rewind($filepointer);
+        rewind($filepointer);
 
-while (($line = fgetcsv($filepointer)) !== false) {
-    print_r($line);
-    echo '<br>';
-}
+        while (($line = fgetcsv($filepointer)) !== false) {
+            print_r($line);
+            echo '<br>';
+        }
 
-fclose($filepointer);
+        fclose($filepointer);
 
-@endphp
+    @endphp
 
-</x-layout>
+</x-app-layout>
