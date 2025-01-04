84183728455ecfe012db628cab2a3343c4a949ec
Jean Sacren <sakiwit@gmail.com>
2025-01-03 21:01:12 -0700

    Use app-layout for datetime view
    Use app-layout in lieu of custom layout for datetime view.
    
    Replace title slot with header slot and format the whole view.

--- resources/views/page/datetime.blade.php
+++ resources/views/page/datetime.blade.php
@@ -1,64 +1,64 @@
 @php
-declare(strict_types=1);
+    declare(strict_types=1);
 @endphp
 
-<x-layout>
+<x-app-layout>
 
-<x-slot:title>
-Datetime
-</x-slot>
+    <x-slot:header>
+        Datetime
+    </x-slot:header>
 
-@php
+    @php
 
-/** Date & Time */
+        /** Date & Time */
 
-date_default_timezone_set('America/Denver'); // set timezone first
+        date_default_timezone_set('America/Denver'); // set timezone first
 
-echo date_default_timezone_get() . '<br>'; // get timezone
+        echo date_default_timezone_get() . '<br>'; // get timezone
 
-$currentTime = time(); // Unix timestamp
+        $currentTime = time(); // Unix timestamp
 
-echo $currentTime . '<br>';
+        echo $currentTime . '<br>';
 
-$fiveDaysFromNow = time() + 5 * 24 * 60 * 60; // 5 days from now
+        $fiveDaysFromNow = time() + 5 * 24 * 60 * 60; // 5 days from now
 
-echo $fiveDaysFromNow . '<br>';
+        echo $fiveDaysFromNow . '<br>';
 
-echo $currentTime - 1 * 60 * 60 * 24 . '<br>'; // 1 day ago
+        echo $currentTime - 1 * 60 * 60 * 24 . '<br>'; // 1 day ago
 
-echo date('m/d/Y g:ia', $currentTime) . '<br>'; // use date() to format a timestamp
+        echo date('m/d/Y g:ia', $currentTime) . '<br>'; // use date() to format a timestamp
 
-echo date('m/d/Y g:ia', $fiveDaysFromNow) . '<br>';
+        echo date('m/d/Y g:ia', $fiveDaysFromNow) . '<br>';
 
-date_default_timezone_set('UTC'); // set timezone back to UTC
+        date_default_timezone_set('UTC'); // set timezone back to UTC
 
-echo date_default_timezone_get() . '<br>';
+        echo date_default_timezone_get() . '<br>';
 
-echo date('m/d/Y g:ia', $currentTime) . '<br>';
+        echo date('m/d/Y g:ia', $currentTime) . '<br>';
 
-$timestamp = mktime(0, 0, 0, 4, 12, null);
+        $timestamp = mktime(0, 0, 0, 4, 12, null);
 
-echo date('m/d/Y g:ia', $timestamp) . '<br>';
+        echo date('m/d/Y g:ia', $timestamp) . '<br>';
 
-$timestamp = strtotime('2021-04-12 08:00:00');
+        $timestamp = strtotime('2021-04-12 08:00:00');
 
-echo date('m/d/Y g:ia', $timestamp) . '<br>';
+        echo date('m/d/Y g:ia', $timestamp) . '<br>';
 
-$timestamp = strtotime('tomorrow');
+        $timestamp = strtotime('tomorrow');
 
-echo date('m/d/Y g:ia', $timestamp) . '<br>';
+        echo date('m/d/Y g:ia', $timestamp) . '<br>';
 
-$timestamp = strtotime('first tuesday of may 2021');
+        $timestamp = strtotime('first tuesday of may 2021');
 
-echo date('m/d/Y g:ia', $timestamp) . '<br>';
+        echo date('m/d/Y g:ia', $timestamp) . '<br>';
 
-$date = date('m/d/Y g:ia', strtotime('second friday of may 2021'));
+        $date = date('m/d/Y g:ia', strtotime('second friday of may 2021'));
 
-echo '<pre>';
-print_r(date_parse($date));
-print_r(date_parse_from_format('m/d/Y g:ia', $date));
-echo '</pre>';
+        echo '<pre>';
+        print_r(date_parse($date));
+        print_r(date_parse_from_format('m/d/Y g:ia', $date));
+        echo '</pre>';
 
-@endphp
+    @endphp
 
-</x-layout>
+</x-app-layout>
