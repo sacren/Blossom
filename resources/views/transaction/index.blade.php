822f00eaa9ae8b5249655bbf51895b9f6f389ab9
Jean Sacren <sakiwit@gmail.com>
2025-01-12 20:51:43 -0700

    Use PHP dynamic variable
    The variable $transaction is dynamic and it is declared in two different
    contexts:
    
    (1) As a class name reference (when used like new $transaction)
    (2) As a regular variable (when assigned values)
    
    Thus, use $transaction variable to refer to both class and instance in
    different contexts.

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -4,23 +4,20 @@
     </x-slot:header>
 
     @php
-        $transact = new $transaction(
-            amount: 100.00,
-            description: 'Test transaction',
-        );
-        $transact->addTax(8.25)->applyDiscount(10);
+        $transaction = new $transaction(amount: 100.0, description: 'Test transaction');
+        $transaction->addTax(8.25)->applyDiscount(10);
 
-        var_dump(number_format($transact->getAmount(), 2));
+        var_dump(number_format($transaction->getAmount(), 2));
         echo '<br>';
 
-        var_dump($transact->getCustomer()?->getPaymentProfile()?->id ?? 'N/A');
+        var_dump($transaction->getCustomer()?->getPaymentProfile()?->id ?? 'N/A');
         echo '<br>';
 
         // object(App\Blossom\Paddle\Transaction)#298 (0) { }
         var_dump(new $paddleTransaction());
         echo '<br>';
 
-        $transact = null; // Destruct: Test transaction
+        $transaction = null; // Destruct: Test transaction
     @endphp
 
 </x-app-layout>
