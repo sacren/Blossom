d3f2c32c99070df59cd4537fdf32d730e5b0259f
Jean Sacren <sakiwit@gmail.com>
2025-01-21 15:43:43 -0700

    Update array view with app-layout component
    Rebuild the view using app-layout component.
    
    Update the app-layout component using $title for the dynamic page title.

--- resources/views/page/array.blade.php
+++ resources/views/page/array.blade.php
@@ -1,92 +1,92 @@
 @php
-declare(strict_types=1);
+    declare(strict_types=1);
 @endphp
 
 @include('page.helper')
 
-<x-layout>
+<x-app-layout>
 
-  <x-slot:title>
-    Array
-  </x-slot>
+    <x-slot:title>
+        Array
+    </x-slot:title>
 
-@php
+    @php
 
-/** Array */
+        /** Array */
 
-$list = [
-  'a' => 1,
-  'b' => 2,
-  'c' => 3,
-  'd' => 4,
-  'e' => 5,
-];
+        $list = [
+            'a' => 1,
+            'b' => 2,
+            'c' => 3,
+            'd' => 4,
+            'e' => 5,
+        ];
 
-prettyPrint($list);
-prettyPrint(array_chunk($list, 3));
-prettyPrint(array_chunk($list, 3, true));
+        prettyPrint($list);
+        prettyPrint(array_chunk($list, 3));
+        prettyPrint(array_chunk($list, 3, true));
 
-$a1 = [ 'a', 'b', 'c' ];
-$a2 = [ 5, 10, 15 ];
+        $a1 = ['a', 'b', 'c'];
+        $a2 = [5, 10, 15];
 
-prettyPrint(array_combine($a1, $a2)); // array size must be equal
+        prettyPrint(array_combine($a1, $a2)); // array size must be equal
 
-$a3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-$a4 = array_filter($a3, fn($value) => $value % 2 === 0); // remove odd numbers
-$a5 = array_filter($a3, fn($key) => $key % 2 === 0, ARRAY_FILTER_USE_KEY);
-$a6 = array_filter($a3, fn($value, $key) => $value % 2 === 0, ARRAY_FILTER_USE_BOTH);
+        $a3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $a4 = array_filter($a3, fn($value) => $value % 2 === 0); // remove odd numbers
+        $a5 = array_filter($a3, fn($key) => $key % 2 === 0, ARRAY_FILTER_USE_KEY);
+        $a6 = array_filter($a3, fn($value, $key) => $value % 2 === 0, ARRAY_FILTER_USE_BOTH);
 
-prettyPrint($a4);
-prettyPrint($a5);
-prettyPrint($a6);
-prettyPrint(array_values($a6)); // reindex array
+        prettyPrint($a4);
+        prettyPrint($a5);
+        prettyPrint($a6);
+        prettyPrint(array_values($a6)); // reindex array
 
-$a7 = [ null, 1, 2, 0.0, '0', 5, false, [], 8, 0, ];
-$a8 = array_filter($a7); // filter out false values by default
+        $a7 = [null, 1, 2, 0.0, '0', 5, false, [], 8, 0];
+        $a8 = array_filter($a7); // filter out false values by default
 
-prettyPrint($a7);
-prettyPrint($a8);
+        prettyPrint($a7);
+        prettyPrint($a8);
 
-$a9 = [ 'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => '1', ];
-$a10 = array_keys($a9);
-$a11 = array_keys($a9, 1);
-$a12 = array_keys($a9, 1, true); // strict comparison
+        $a9 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => '1'];
+        $a10 = array_keys($a9);
+        $a11 = array_keys($a9, 1);
+        $a12 = array_keys($a9, 1, true); // strict comparison
 
-prettyPrint($a9);
-prettyPrint($a10);
-prettyPrint($a11);
-prettyPrint($a12);
+        prettyPrint($a9);
+        prettyPrint($a10);
+        prettyPrint($a11);
+        prettyPrint($a12);
 
-$a13 = [ 1, 2, 3, ];
-// callback takes array value for argument
-// index retained
-$a14 = array_map(fn($value) => $value + 2, $a13);
+        $a13 = [1, 2, 3];
+        // callback takes array value for argument
+        // index retained
+        $a14 = array_map(fn($value) => $value + 2, $a13);
 
-prettyPrint($a13);
-prettyPrint($a14);
+        prettyPrint($a13);
+        prettyPrint($a14);
 
-$a15 = [ 'a' => 1, 'b' => 2, 'c' => 3, ];
-$a16 = [ 'd' => 4, 'e' => 5, 'f' => 6, ];
-$a17 = array_map(fn($value) => $value + 2, $a15); // index retained
-// callback takes two array values for arguments
-// new index
-$a18 = array_map(fn($v1, $v2) => $v1 + $v2, $a15, $a16);
+        $a15 = ['a' => 1, 'b' => 2, 'c' => 3];
+        $a16 = ['d' => 4, 'e' => 5, 'f' => 6];
+        $a17 = array_map(fn($value) => $value + 2, $a15); // index retained
+        // callback takes two array values for arguments
+        // new index
+        $a18 = array_map(fn($v1, $v2) => $v1 + $v2, $a15, $a16);
 
-prettyPrint($a15);
-prettyPrint($a16);
-prettyPrint($a17);
-prettyPrint($a18);
+        prettyPrint($a15);
+        prettyPrint($a16);
+        prettyPrint($a17);
+        prettyPrint($a18);
 
-// not aligned with $a16, 0 will be added
-$a19 = [ 'a' => 1, 'b' => 2, ];
-$a20 = array_map(fn($v1, $v2) => $v1 + $v2, $a19, $a16);
+        // not aligned with $a16, 0 will be added
+        $a19 = ['a' => 1, 'b' => 2];
+        $a20 = array_map(fn($v1, $v2) => $v1 + $v2, $a19, $a16);
 
-prettyPrint($a20);
+        prettyPrint($a20);
 
-$a21 = array_map(null, $a15, $a16); // build new array
+        $a21 = array_map(null, $a15, $a16); // build new array
 
-prettyPrint($a21);
+        prettyPrint($a21);
 
-@endphp
+    @endphp
 
-</x-layout>
+</x-app-layout>
